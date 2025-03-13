71c3f02a088135d691ae4d84a9469e20b3eb9bc4
Stefano Esposito <stefano.esposito97@outlook.com>
2025-03-13 15:55:39 +0100

    update

--- build.py
+++ build.py
@@ -1,11 +1,8 @@
 from pybtex.database.input import bibtex
 
+
 def get_personal_data():
     name = ["Stefano", "Esposito"]
-    email = "stefano.esposito97@outlook.com"
-    twitter = "StefanoEsp"
-    github = "s-esposito"
-    linkedin = "stefanoesposito97"
     bio_text = f"""
                 <p>I am a PhD student at the <a href="https://uni-tuebingen.de/en/">University of Tübingen</a>, part of <a href="https://uni-tuebingen.de/en/fakultaeten/mathematisch-naturwissenschaftliche-fakultaet/fachbereiche/informatik/lehrstuehle/autonomous-vision/home/">Autonomous Vision Group</a> headed by <a href="https://www.cvlibs.net/">Prof. Andreas Geiger</a>.</p>
                 <p>
@@ -20,7 +17,7 @@ def get_personal_data():
                 <p>
                     <a href="https://s-esposito.github.io/assets/pdf/Stefano_Esposito_CV.pdf" target="_blank" style="margin-right: 15px"><i class="fa fa-address-card fa-lg"></i> CV</a>
                     <a href="mailto:stefano.esposito97@outlook.com" style="margin-right: 15px"><i class="far fa-envelope-open fa-lg"></i> Mail</a>
-                    <a href="https://twitter.com/StefanoEsp" target="_blank" style="margin-right: 15px"><i class="fab fa-twitter fa-lg"></i> Twitter</a>
+                    <a href="https://bsky.app/profile/s-esposito.bsky.social" target="_blank" style="margin-right: 15px"><i class="fa-brands fa-bluesky"></i> Bluesky</a>
                     <a href="https://scholar.google.com/citations?user=5RhJ-eEAAAAJ&hl=it" target="_blank" style="margin-right: 15px"><i class="fa-solid fa-book"></i> Scholar</a>
                     <a href="https://github.com/s-esposito" target="_blank" style="margin-right: 15px"><i class="fab fa-github fa-lg"></i> Github</a>
                     <a href="https://www.linkedin.com/in/stefanoesposito97" target="_blank" style="margin-right: 15px"><i class="fab fa-linkedin fa-lg"></i> LinkedIn</a>
@@ -38,61 +35,71 @@ def get_personal_data():
     """
     return name, bio_text, footer
 
+
 def get_author_dict():
     return {
-        'Andreas Geiger': 'https://www.cvlibs.net/',
-        'Songyou Peng': 'https://pengsongyou.github.io/',
-        'Zehao Yu': 'https://niujinshuchong.github.io/',
-        'Torsten Sattler': 'https://tsattler.github.io/',
-        'Katja Schwarz': 'https://katjaschwarz.github.io/',
-        'Axel Sauer': 'https://axelsauer.com/',
-        'Jonathan Barron': 'https://jonbarron.info/',
-        'Ben Mildenhall': 'https://bmild.github.io/',
-        'Mehdi Sajjadi': 'https://msajjadi.com/',
-        'Noha Radwan': 'http://www2.informatik.uni-freiburg.de/~radwann/',
-        'Chiyu Jiang': 'https://www.maxjiang.ml/',
-        'Yiyi Liao': 'https://yiyiliao.github.io/',
-        'Marc Pollefeys': 'https://people.inf.ethz.ch/pomarc/',
-        'Michael Oechsle': 'https://moechsle.github.io/',
-        'Christian Reiser': 'https://creiser.github.io/',
-        'Lars Mescheder': 'https://scholar.google.de/citations?user=h2k1gL4AAAAJ&hl=de',
-        'Thilo Strauss': 'https://scholar.google.com/citations?user=VlymtLQAAAAJ&hl=en',
-        'Sebastian Nowozin': 'http://www.nowozin.net/sebastian/',
-        'Marie-Julie Rakotosaona': 'http://www.lix.polytechnique.fr/Labo/Marie-Julie.RAKOTOSAONA/',
-        'Fabian Manhardt': 'https://campar.in.tum.de/Main/FabianManhardt',
-        'Diego Martin Arroyo': 'https://martinarroyo.net/',
-        'Abhijit Kundu': 'https://abhijitkundu.info/',
-        'Federico Tombari': 'https://www.cs.cit.tum.de/camp/members/senior-research-scientists/federico-tombari/',
-        'Anpei Chen': 'https://apchenstu.github.io/',
-        'Bozidar Antic': 'https://bozidarantic.com/',
-        'Apratim Bhattacharyya': 'https://apratimbhattacharyya18.github.io/',
-        'Siyu Tang': 'https://inf.ethz.ch/people/person-detail.MjYyNzgw.TGlzdC8zMDQsLTg3NDc3NjI0MQ==.html',
-        'Daniele Baieri': 'https://gladia.di.uniroma1.it/authors/baieri/',
-        'Stefan Zellmann': 'https://www.szellmann.de/',
-        'André Hinkenjann': 'https://www.h-brs.de/en/inf/prof-dr-andre-hinkenjann',
-        'Emanuele Rodolà': 'https://gladia.di.uniroma1.it/authors/rodola/',
-        'Filippo Maggioli': 'https://gladia.di.uniroma1.it/authors/maggioli/',
-        'Samuel Rota Bulò': 'https://scholar.google.com/citations?user=484sccEAAAAJ&hl=it', 
-        'Lorenzo Porzi': 'https://scholar.google.it/citations?user=vW1gaVEAAAAJ&hl=it',
-        'Christian Richardt': 'https://richardt.name/', 
-        'Michael Zollhoefer': 'https://zollhoefer.com/',
-        'Peter Kontschieder': 'https://scholar.google.co.uk/citations?user=CxbDDRMAAAAJ&hl=en',
-        'Haofei Xu': 'https://haofeixu.github.io/',
-        'Siyu Tang': 'https://inf.ethz.ch/people/person-detail.MjYyNzgw.TGlzdC8zMDQsLTg3NDc3NjI0MQ==.html',
-        'Donato Crisostomi': 'https://crisostomi.com/'
-        }
+        "Andreas Geiger": "https://www.cvlibs.net/",
+        "Songyou Peng": "https://pengsongyou.github.io/",
+        "Zehao Yu": "https://niujinshuchong.github.io/",
+        "Torsten Sattler": "https://tsattler.github.io/",
+        "Katja Schwarz": "https://katjaschwarz.github.io/",
+        "Axel Sauer": "https://axelsauer.com/",
+        "Jonathan Barron": "https://jonbarron.info/",
+        "Ben Mildenhall": "https://bmild.github.io/",
+        "Mehdi Sajjadi": "https://msajjadi.com/",
+        "Noha Radwan": "http://www2.informatik.uni-freiburg.de/~radwann/",
+        "Chiyu Jiang": "https://www.maxjiang.ml/",
+        "Yiyi Liao": "https://yiyiliao.github.io/",
+        "Marc Pollefeys": "https://people.inf.ethz.ch/pomarc/",
+        "Michael Oechsle": "https://moechsle.github.io/",
+        "Christian Reiser": "https://creiser.github.io/",
+        "Lars Mescheder": "https://scholar.google.de/citations?user=h2k1gL4AAAAJ&hl=de",
+        "Thilo Strauss": "https://scholar.google.com/citations?user=VlymtLQAAAAJ&hl=en",
+        "Sebastian Nowozin": "http://www.nowozin.net/sebastian/",
+        "Marie-Julie Rakotosaona": "http://www.lix.polytechnique.fr/Labo/Marie-Julie.RAKOTOSAONA/",
+        "Fabian Manhardt": "https://campar.in.tum.de/Main/FabianManhardt",
+        "Diego Martin Arroyo": "https://martinarroyo.net/",
+        "Abhijit Kundu": "https://abhijitkundu.info/",
+        "Federico Tombari": "https://www.cs.cit.tum.de/camp/members/senior-research-scientists/federico-tombari/",
+        "Anpei Chen": "https://apchenstu.github.io/",
+        "Bozidar Antic": "https://bozidarantic.com/",
+        "Apratim Bhattacharyya": "https://apratimbhattacharyya18.github.io/",
+        "Siyu Tang": "https://inf.ethz.ch/people/person-detail.MjYyNzgw.TGlzdC8zMDQsLTg3NDc3NjI0MQ==.html",
+        "Daniele Baieri": "https://gladia.di.uniroma1.it/authors/baieri/",
+        "Stefan Zellmann": "https://www.szellmann.de/",
+        "André Hinkenjann": "https://www.h-brs.de/en/inf/prof-dr-andre-hinkenjann",
+        "Emanuele Rodolà": "https://gladia.di.uniroma1.it/authors/rodola/",
+        "Filippo Maggioli": "https://gladia.di.uniroma1.it/authors/maggioli/",
+        "Samuel Rota Bulò": "https://scholar.google.com/citations?user=484sccEAAAAJ&hl=it",
+        "Lorenzo Porzi": "https://scholar.google.it/citations?user=vW1gaVEAAAAJ&hl=it",
+        "Christian Richardt": "https://richardt.name/",
+        "Michael Zollhoefer": "https://zollhoefer.com/",
+        "Peter Kontschieder": "https://scholar.google.co.uk/citations?user=CxbDDRMAAAAJ&hl=en",
+        "Haofei Xu": "https://haofeixu.github.io/",
+        "Siyu Tang": "https://inf.ethz.ch/people/person-detail.MjYyNzgw.TGlzdC8zMDQsLTg3NDc3NjI0MQ==.html",
+        "Donato Crisostomi": "https://crisostomi.com/",
+    }
 
-def generate_person_html(persons, connection=", ", make_bold=True, make_bold_name='Michael Niemeyer', add_links=True):
+
+def generate_person_html(
+    persons,
+    connection=", ",
+    make_bold=True,
+    make_bold_name="Michael Niemeyer",
+    add_links=True,
+):
     links = get_author_dict() if add_links else {}
     s = ""
     for p in persons:
         string_part_i = ""
-        for name_part_i in p.get_part('first') + p.get_part('last'): 
+        for name_part_i in p.get_part("first") + p.get_part("last"):
             if string_part_i != "":
                 string_part_i += " "
             string_part_i += name_part_i
         if string_part_i in links.keys():
-            string_part_i = f'<a href="{links[string_part_i]}" target="_blank">{string_part_i}</a>'
+            string_part_i = (
+                f'<a href="{links[string_part_i]}" target="_blank">{string_part_i}</a>'
+            )
         if make_bold and string_part_i == make_bold_name:
             string_part_i = f'<span style="font-weight: bold";>{make_bold_name}</span>'
         if p != persons[-1]:
@@ -100,38 +107,63 @@ def generate_person_html(persons, connection=", ", make_bold=True, make_bold_nam
         s += string_part_i
     return s
 
+
 def get_paper_entry(entry_key, entry):
     s = """<div style="margin-bottom: 2em;"> <div class="row"><div class="col-sm-3">"""
     s += f"""<img src="{entry.fields['img']}" class="img-fluid custom-img-thumbnail" alt="Project image">"""
     s += """</div><div class="col-sm-9">"""
 
-    if 'award' in entry.fields.keys():
+    if "award" in entry.fields.keys():
         s += f"""<a href="{entry.fields['html']}" target="_blank">{entry.fields['title']}</a> <span style="color: red;">({entry.fields['award']})</span><br>"""
     else:
         s += f"""<a href="{entry.fields['html']}" target="_blank">{entry.fields['title']}</a> <br>"""
 
     s += f"""{generate_person_html(entry.persons['author'])} <br>"""
-    s += f"""<span style="font-style: italic;">{entry.fields['booktitle']}</span>, {entry.fields['year']} <br>"""
+    if "booktitle" in entry.fields.keys():
+        s += f"""<span style="font-style: italic;">{entry.fields['booktitle']}</span>, {entry.fields['year']} <br>"""
+    elif "note" in entry.fields.keys():
+        s += f"""<span style="font-style: italic;">{entry.fields['note']}</span>, {entry.fields['year']} <br>"""
+    else:
+        s += f"""{entry.fields['year']} <br>"""
 
-    artefacts = {'html': 'Project Page', 'pdf': 'Paper', 'supp': 'Supplemental', 'video': 'Video', 'poster': 'Poster', 'code': 'Code'}
+    artefacts = {
+        "html": "Project Page",
+        "pdf": "Paper",
+        "supp": "Supplemental",
+        "video": "Video",
+        "poster": "Poster",
+        "code": "Code",
+    }
     i = 0
-    for (k, v) in artefacts.items():
+    for k, v in artefacts.items():
         if k in entry.fields.keys():
             if i > 0:
-                s += ' / '
+                s += " / "
             s += f"""<a href="{entry.fields[k]}" target="_blank">{v}</a>"""
             i += 1
         else:
-            print(f'[{entry_key}] Warning: Field {k} missing!')
+            print(f"[{entry_key}] Warning: Field {k} missing!")
 
     cite = "<pre><code>@InProceedings{" + f"{entry_key}, \n"
-    cite += "\tauthor = {" + f"{generate_person_html(entry.persons['author'], make_bold=False, add_links=False, connection=' and ')}" + "}, \n"
-    for entr in ['title', 'booktitle', 'year']:
+    cite += (
+        "\tauthor = {"
+        + f"{generate_person_html(entry.persons['author'], make_bold=False, add_links=False, connection=' and ')}"
+        + "}, \n"
+    )
+    if "booktitle" in entry.fields.keys():
+        cite += "\tbooktitle = " + "{" + f"{entry.fields['booktitle']}" + "}, \n"
+    if "note" in entry.fields.keys():
+        cite += "\tnote = " + "{" + f"{entry.fields['note']}" + "}, \n"
+    for entr in ["title", "year"]:
         cite += f"\t{entr} = " + "{" + f"{entry.fields[entr]}" + "}, \n"
     cite += """}</pre></code>"""
-    s += " /" + f"""<button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse{entry_key}" aria-expanded="false" aria-controls="collapseExample" style="margin-left: -6px; margin-top: -2px;">Expand bibtex</button><div class="collapse" id="collapse{entry_key}"><div class="card card-body">{cite}</div></div>"""
+    s += (
+        " /"
+        + f"""<button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse{entry_key}" aria-expanded="false" aria-controls="collapseExample" style="margin-left: -6px; margin-top: -2px;">Expand bibtex</button><div class="collapse" id="collapse{entry_key}"><div class="card card-body">{cite}</div></div>"""
+    )
     s += """ </div> </div> </div>"""
     return s
+
 
 def get_talk_entry(entry_key, entry):
     s = """<div style="margin-bottom: 2em;"> <div class="row"><div class="col-sm-3">"""
@@ -140,36 +172,39 @@ def get_talk_entry(entry_key, entry):
     s += f"""{entry.fields['title']}<br>"""
     s += f"""<span style="font-style: italic;">{entry.fields['booktitle']}</span>, {entry.fields['year']} <br>"""
 
-    artefacts = {'slides': 'Slides', 'video': 'Recording'}
+    artefacts = {"slides": "Slides", "video": "Recording"}
     i = 0
-    for (k, v) in artefacts.items():
+    for k, v in artefacts.items():
         if k in entry.fields.keys():
             if i > 0:
-                s += ' / '
+                s += " / "
             s += f"""<a href="{entry.fields[k]}" target="_blank">{v}</a>"""
             i += 1
         else:
-            print(f'[{entry_key}] Warning: Field {k} missing!')
+            print(f"[{entry_key}] Warning: Field {k} missing!")
     s += """ </div> </div> </div>"""
     return s
 
+
 def get_publications_html():
     parser = bibtex.Parser()
-    bib_data = parser.parse_file('publication_list.bib')
+    bib_data = parser.parse_file("publication_list.bib")
     keys = bib_data.entries.keys()
     s = ""
     for k in keys:
-        s+= get_paper_entry(k, bib_data.entries[k])
+        s += get_paper_entry(k, bib_data.entries[k])
     return s
+
 
 def get_talks_html():
     parser = bibtex.Parser()
-    bib_data = parser.parse_file('talk_list.bib')
+    bib_data = parser.parse_file("talk_list.bib")
     keys = bib_data.entries.keys()
     s = ""
     for k in keys:
-        s+= get_talk_entry(k, bib_data.entries[k])
+        s += get_talk_entry(k, bib_data.entries[k])
     return s
+
 
 def get_index_html():
     pub = get_publications_html()
@@ -257,11 +292,12 @@ def get_index_html():
     return s
 
 
-def write_index_html(filename='index.html'):
+def write_index_html(filename="index.html"):
     s = get_index_html()
-    with open(filename, 'w', encoding='utf-8') as f:
+    with open(filename, "w", encoding="utf-8") as f:
         f.write(s)
-    print(f'Written index content to {filename}.')
+    print(f"Written index content to {filename}.")
 
-if __name__ == '__main__':
-    write_index_html('index.html')
+
+if __name__ == "__main__":
+    write_index_html("index.html")
